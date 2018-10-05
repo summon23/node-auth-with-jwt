ab18d72be251b409ef8cc72e36e4256062aa5c4e
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-10-05 23:48:51 +0700

    seperate api routes

--- app/index.js
+++ app/index.js
@@ -1,34 +1,27 @@
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-var jwt = require('jsonwebtoken');
-var config = require('../config');
-var User = require('../app/models/user');
+const config = require('../config');
+const User = require('../app/models/user');
+const ApiRoutes = require('./routes/api_routes');
 
-
-var port = process.env.PORT || 3010;
 mongoose.connect(config.database, { useMongoClient: true });
 app.set('superSecret', config.secret); //replace express superSecret with app key
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(morgan('dev'));
 
-app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
 
 app.get('/', function (req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
-
 app.get('/setup', function (req, res) {
-    var nick = new User({
+    const nick = new User({
         name: 'nick cerminara',
         password: 'password',
         admin: true
@@ -42,81 +35,6 @@ app.get('/setup', function (req, res) {
     })
 });
 
-//API Routes
-var apiRoutes = express.Router();
-
-apiRoutes.post('/auth', function (req, res) {
-    User.findOne({
-        name: req.body.name
-    }, function (err, user) {
-        if (err) throw err;
-
-        if (!user) {
-            res.json({
-                success: false,
-                message: 'Auth failed, User not found'
-            });
-        } else {
-            //check if user matched with database
-            if (user.password != req.body.password) {
-                res.json({
-                    success: false,
-                    message: 'Auth failed, User or Password Wrong'
-                });
-            } else {
-                const payload = {
-                    admin: user.admin
-                };
-
-                var token = jwt.sign(payload, app.get('superSecret'), {
-                    expiresIn: '1440d'
-                });
-
-                res.json({
-                    success: true,
-                    message: 'Enjoy your token',
-                    token: token
-                });
-            }
-        }
-    })
-});
-
-apiRoutes.use(function (req, res, next) {
-    console.log('middleware verify');
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    //decode the token
-    if (token) {
-        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
-            if (err) {
-                return res.json({
-                    success: false,
-                    message: 'Failed to auth token'
-                })
-            } else {
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else {
-        return res.status(403).send({
-            success: false,
-            message: 'No Token provided'
-        });
-    }
-});
-
-apiRoutes.get('/', function (req, res) {
-    res.json({ message: 'Welcome to the coolest API on earth' });
-});
-
-apiRoutes.get('/users', function (req, res) {
-    User.find({}, function (err, users) {
-        res.json(users);
-    });
-});
-
-app.use('/api', apiRoutes);
+app.use('/api', ApiRoutes);
 
 module.exports = app;
---
+++ app/routes/api_routes.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+router.post('/auth', function (req, res) {
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+        if (err) throw err;
+
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Auth failed, User not found'
+            });
+        } else {
+            //check if user matched with database
+            if (user.password != req.body.password) {
+                res.json({
+                    success: false,
+                    message: 'Auth failed, User or Password Wrong'
+                });
+            } else {
+                const payload = {
+                    admin: user.admin
+                };
+
+                var token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: '1440d'
+                });
+
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token',
+                    token: token
+                });
+            }
+        }
+    })
+});
+
+router.use(function (req, res, next) {
+    console.log('middleware verify');
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    //decode the token
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Failed to auth token'
+                })
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No Token provided'
+        });
+    }
+});
+
+router.get('/', function (req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth' });
+});
+
+router.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
+        res.json(users);
+    });
+});
+
+module.exports = router;
