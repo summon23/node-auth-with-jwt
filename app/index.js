f84de544c8e0a4cbcadafcd64f74ea317ffed9fe
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-10-05 23:58:25 +0700

    move all routes into folders

--- app/index.js
+++ app/index.js
@@ -6,35 +6,14 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const config = require('../config');
-const User = require('../app/models/user');
-const ApiRoutes = require('./routes/api_routes');
+const Routes = require('./routes');
 
 mongoose.connect(config.database, { useMongoClient: true });
-app.set('superSecret', config.secret); //replace express superSecret with app key
+app.set('superSecretKey', config.secret);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-
-app.get('/', function (req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
-
-app.get('/setup', function (req, res) {
-    const nick = new User({
-        name: 'nick cerminara',
-        password: 'password',
-        admin: true
-    });
-
-    nick.save(function (err) {
-        if (err) throw err;
-
-        console.log('User saved successfully');
-        res.json({ success: true });
-    })
-});
-
-app.use('/api', ApiRoutes);
+Routes(app);
 
 module.exports = app;
--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -2,7 +2,32 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-router.post('/auth', function (req, res) {
+router.use(function (req, res, next) {
+    console.log('middleware verify');
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    //decode the token
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Failed to auth token'
+                })
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No Token provided'
+        });
+    }
+});
+
+router.post('/auth', function (req, res, next) {
     User.findOne({
         name: req.body.name
     }, function (err, user) {
@@ -39,36 +64,11 @@ router.post('/auth', function (req, res) {
     })
 });
 
-router.use(function (req, res, next) {
-    console.log('middleware verify');
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    //decode the token
-    if (token) {
-        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
-            if (err) {
-                return res.json({
-                    success: false,
-                    message: 'Failed to auth token'
-                })
-            } else {
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else {
-        return res.status(403).send({
-            success: false,
-            message: 'No Token provided'
-        });
-    }
-});
-
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
     res.json({ message: 'Welcome to the coolest API on earth' });
 });
 
-router.get('/users', function (req, res) {
+router.get('/users', function (req, res, next) {
     User.find({}, function (err, users) {
         res.json(users);
     });
